948819793fec328740d42a3bf60d2d173af13c35
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2013-03-05 23:35:40 -0500

    Add option --level

--- journalnoti.py
+++ journalnoti.py
@@ -1,6 +1,17 @@
+'''Pop up notifier icons when journal events happen'''
+
 import sys
 import dbus
+import argparse
 from systemd import journal
+
+__version__ = '0.0.1'
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--version', action='version',
+                    version=__version__)
+parser.add_argument('-l', '--level', type=int, choices=range(8),
+                    help='Filter messages by priority')
 
 def notify(summary, body='', app_name='', app_icon='edit-cut',
            timeout=5000, actions=[], hints=[], replaces_id=0):
@@ -11,7 +22,13 @@ def notify(summary, body='', app_name='', app_icon='edit-cut',
     interface = dbus.Interface(obj, 'org.freedesktop.Notifications')
     interface.Notify(app_name, replaces_id, app_icon,
                      summary, body, actions, hints, timeout)
-                               
+
+def create_reader(opts):
+    r = journal.Reader()
+    if opts.level:
+        r.log_level(opts.level)
+    return r
+
 def loop(reader):
     while True:
         for event in reader:
@@ -19,6 +36,7 @@ def loop(reader):
         reader.wait()
 
 if __name__ == '__main__':
-    r = journal.Reader()
+    opts = parser.parse_args()
+    r = create_reader(opts)
     r.seek(0, journal.SEEK_END)
     loop(r)
